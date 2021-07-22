242b6ebb895f966ecc8341608fa44be772f021da
woody-pet <woody.pet.2021@gmail.com>
2021-07-22 18:00:14 +0800

    tomcat url支持特殊字符。400与500错误页面

--- src/main/java/com/gemini/WoodyApplication.java
+++ src/main/java/com/gemini/WoodyApplication.java
@@ -10,4 +10,5 @@ public class WoodyApplication {
         SpringApplication.run(WoodyApplication.class, args);
     }
 
+
 }
---
+++ src/main/java/com/gemini/config/TomcatConfig.java
@@ -0,0 +1,24 @@
+package com.gemini.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    public TomcatServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                connector.setProperty("relaxedPathChars", "\"<>[\\]^`{|}");
+                connector.setProperty("relaxedQueryChars", "\"<>[\\]^`{|}");
+            }
+        });
+        return factory;
+    }
+}
