6aa2b83ac5f8c83dd34d1633c24c824c6fdbbe1e
woody-pet <woody.pet.2021@gmail.com>
2021-07-24 17:09:03 +0800

    druid配置放入-test.yml

--- HELP.md
+++ HELP.md
@@ -6,6 +6,11 @@
 
 * ssh 192.168.1.100 22 root root
 * mysql 192.168.1.100 3306 root Hi.1234567
+* docker mysql 192.168.1.100 3307 root 123456
 * jenkins http://192.168.1.100:8080/ wuchao wuchao
 * webapp http://192.168.1.100:8081/
 * github https://github.com/woody-pet/woody/ woody-pet Hi.1234567
+
+
+docker run -p 3307:3306 --name mysql01 --privileged=true -v /usr/local/docker/mysql/conf:/etc/mysql -v /usr/local/docker/mysql/logs:/var/log/mysql -v /usr/local/docker/mysql/data:/var/lib/mysql -v /usr/local/docker/mysql/mysql-files:/var/lib/mysql-files -e MYSQL_ROOT_PASSWORD=123456 -d mysql:8.0
+firewall-cmd --zone=public --add-port=80/tcp --permanent
--- src/test/java/com/gemini/WoodyApplicationTests.java
+++ src/test/java/com/gemini/WoodyApplicationTests.java
@@ -1,13 +1,34 @@
 package com.gemini;
 
+import com.gemini.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+//@RunWith
 @SpringBootTest
 class WoodyApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    UserMapper userMapper;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        System.out.println(dataSource.getClass());
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+
+    @Test
+    void mapper() throws Exception {
+        System.out.println(userMapper.getUserById(1));
     }
 
 }
